f7ef591b259ce591a522427260315474df6ec716
Evan Maicus <emaicus@outlook.com>
2019-08-30 10:09:12 -0400

    network_generator added

---
+++ network_generator/README.md
@@ -0,0 +1,49 @@
+#### This application is a simple network generator for use by students taking network programming courses
+
+##### To run:
+  0. Install and configure docker for your operating system. 
+     __NOTE:__ Make certain you are in the docker group on your machine
+  1. Clone this repository to your local machine.
+  2. Install this script's dependencies by running.
+     ```pip3 install -r requirements.txt```
+  3. Configure a docker network within a ```network_config.json``` as described in the Network Configuration section
+  4. Run the ```student_network_generator.py``` script, passing in the path to your ```network_config.json```.
+  5. Run the commands output by the network generation script to be dropped into bash environments within your containers.
+  6. Check regularly to see if new versions of this script have been released.
+  7. __NOTE:__ If you encounter any errors while using the script, please ask about them first on your course forum, then, if it is indeed an error in this script, make an issue in this repository.
+
+
+##### Network Configuration
+  The network configuration has two fields, ```solution_directory``` and ```containers```.
+  1. ```solution_directory``` is a string, and must be a full path to the solution code that you want copied into your container.
+
+  2. ```containers``` is an object, where:
+    a. Each key represents a unique container name which represents an object containing.
+      i.  An ```image``` field, which specifies the docker image to be used in container creation.
+      ii. A ```ports``` field, which contains a positive, nonzero integer number of ports to be assigned to the container.
+
+  Therefore, the following configuration:
+  ```
+  {
+    "solution_directory" : "/path/to/my/solution/code",
+    "containers" : {
+      "alpha" : {
+        "image" : "submittyrpi/csci4510:default",
+        "ports" : 1
+      },
+      "beta" : {
+        "image" : "submittyrpi/csci4510:default",
+        "ports" : 1
+      },
+      "charlie" : {
+        "image" : "submittyrpi/csci4510:default",
+        "ports" : 1
+      }
+    }
+  }
+  ```
+
+  Creates a network with three endpoints, ```alpha```, ```beta```, and ```charlie```, which are instantiated using the ```submittyrpi/csci4510:default``` container image, and which have 1 port in the resultant ```knownhost_tcp.txt``` and ```knownhost_udp.txt``` files.
+
+  __NOTE:__ Only a value of ```1``` is accepted at present by the network generator. Additional ports will be added in a future release.
+  
---
+++ network_generator/student_network_generator.py
@@ -0,0 +1,197 @@
+import os
+import sys
+import docker
+import argparse
+import shutil
+import getpass
+import json
+from distutils.dir_util import copy_tree
+
+
+# True if yes, false if anything else
+def yes_or_no(question):
+  response = input(f'{question} (y/n) ').strip().lower()
+  return True if response in ['y', 'yes'] else False
+
+# Will be supported in a future release
+# def generate_knownhosts_json(containers, name_to_ip, container_directory, tcp_port, udp_port, filename):
+#   knownhosts_dict = dict()
+
+#   for name, info in containers.items():
+#     ports = info.get('number_of_ports', 1)
+
+#     if ports <= 0:
+#       raise SystemExit("ERROR: Each host must be assigned 1 or more ports")
+
+#     ip_address = name_to_ip[name]
+#     knownhosts_dict['hosts'] = dict()
+#     knownhosts_dict['hosts'][name] = dict()
+#     knownhosts_dict['hosts'][name]['tcp_start_port'] = tcp_port
+#     knownhosts_dict['hosts'][name]['tcp_end_port']   = tcp_port + ports - 1
+#     knownhosts_dict['hosts'][name]['udp_start_port'] = udp_port
+#     knownhosts_dict['hosts'][name]['udp_end_port']   = udp_port + ports - 1
+#     knownhosts_dict['hosts'][name]['ip_address']     = ip_address
+    
+#     tcp_port = tcp_port + ports
+#     udp_port = udp_port + ports
+
+#   with open(filename, 'w') as outfile:
+#     json.dump(knownhosts_dict, outfile, indent=4)
+
+def generate_knownhosts_txt(containers, container_directory, starting_port, filename):
+  with open(filename, 'w') as outfile:
+    for name, info in containers.items():
+      ports = info.get('ports', 1)
+
+      if ports <= 0:
+        raise SystemExit("ERROR: Each host must be assigned 1 or more ports")
+
+      if ports > 1:
+        raise SystemExit("ERROR: Multiple ports will be supported in a future release.")
+
+      if ports > 1:
+        outfile.write(f'{name} {starting_port}-{starting_port + ports - 1}\n')
+      else:
+        outfile.write(f'{name} {starting_port}\n')
+
+      starting_port = starting_port + ports
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description='This utility will help you to quickly deploy docker networks to test student assignment submissions')
+  parser.add_argument('network_specification_file',  help="The path to your network specification json.", type=str)
+  args = parser.parse_args()
+
+  working_directory = os.path.join(os.getcwd(), 'WORKING_DIRECTORY')
+  client = docker.from_env()
+  # Get the name of the user running the script to avoid name conflicts w/ other users on the system.
+  username = getpass.getuser()
+
+  if not os.path.exists(args.network_specification_file):
+    raise SystemExit(f"ERROR: it looks like {args.network_specification_file} doesn't exist.")
+
+  with open(args.network_specification_file, 'r') as infile:
+    network_config = json.load(infile)
+
+  if 'solution_directory' not in network_config:
+    raise SystemExit(f"ERROR: please specify the full to your 'solution_directory' in your network config.")
+
+  solution_directory = network_config['solution_directory']
+  network = network_config['containers']
+
+  if not os.path.isdir(solution_directory):
+    raise SystemExit(f"ERROR: it looks like {solution_directory} doesn't exist.")
+
+  if os.path.isdir(working_directory):
+    # A safety feature to avoid removing a directory that the user wants around.
+    # Remove at your own risk.
+    if yes_or_no(f'It looks like {working_directory} already exists. Is it alright to remove it?') == False:
+       raise SystemExit(f"Terminating.")
+    shutil.rmtree(working_directory)
+
+  os.mkdir(working_directory)
+  print('Creating your network...')
+  
+  print('Removing old containers...')  
+  # Clean up docker networks and containers from previous runs
+  for container_name in network.keys():
+    full_container_name = f'{username}_{container_name}'
+    try:
+      old_container = client.containers.get(full_container_name)
+      old_container.remove(force=True)
+    except docker.errors.NotFound:
+      continue
+
+  full_network_name = f'{username}_network'
+  print(full_network_name)
+  
+  try:
+    old_network = client.networks.get(full_network_name)
+    old_network.remove()
+  except docker.errors.NotFound:
+    pass
+
+  # Variables to assign an ip address to each container.
+  network_num = 10
+  user_num = 1
+  subnet = 1
+  error = True
+  while error:
+    # Build a subnet on the docker network. Allows us to assign ip addresses.
+    ipam_pool = docker.types.IPAMPool(
+      subnet=f'{network_num}.{user_num}.{subnet}.0/24',)
+    ipam_config = docker.types.IPAMConfig(pool_configs=[ipam_pool])
+    try:
+      docker_network =  client.networks.create(full_network_name, ipam=ipam_config, driver='bridge', internal=True)
+      error = False
+    except:
+      print(f'Error: could not build the network {network_num}.{user_num}.{subnet}.0/24')
+      user_num += 1
+      print(f'Trying {network_num}.{user_num}.{subnet}.0/24')
+      pass
+
+  print('Creating new containers...')
+  # Host number for a container's ip, starting at 2.
+  host = 2
+  name_to_ip = dict()
+  for container_name, container_info in network.items():
+    image = container_info['image']
+    # We append the user's name to the container name to avoid conflicts w/ other users.
+    full_container_name = f'{username}_{container_name}'
+    # Each container mounts its own directory inside of working_dir
+    container_dir = os.path.join(working_directory, container_name)
+    
+    os.makedirs(container_dir)
+    copy_tree(solution_directory, container_dir)
+
+    mount = {
+      container_dir : {
+        'bind' : container_dir,
+        'mode' : 'rw'
+      }
+    }
+
+    # Create the container
+    container = client.containers.create(image, 
+                                         command ='/bin/bash', 
+                                         stdin_open = True, 
+                                         tty = True,
+                                         volumes=mount,
+                                         working_dir = container_dir, 
+                                         hostname = container_name,
+                                         name=full_container_name
+                                        )
+    # Connect the container to the network with a unique ip address.
+    ip = f'{network_num}.{user_num}.{subnet}.{host}'
+    docker_network.connect(container, ipv4_address=ip, aliases=[container_name,])
+    name_to_ip[container_name] = ip
+    host += 1
+
+  # Generating knownhosts file:
+  for container_name, container_info in network.items():
+    full_container_name = f'{username}_{container_name}'
+    container_dir = os.path.join(working_directory, container_name)
+    # Knownhosts json will be supported in a future release
+    # generate_knownhosts(network, 
+    #                     name_to_ip, 
+    #                     container_dir, 
+    #                     9000, 
+    #                     10000,  
+    #                     os.path.join(container_dir, 'knownhosts.json'))
+
+    # Create network configuration files within the container.
+    generate_knownhosts_txt(network, container_dir, 9000,  os.path.join(container_dir,'knownhosts_tcp.txt'))
+    generate_knownhosts_txt(network, container_dir, 10000, os.path.join(container_dir,'knownhosts_udp.txt'))
+  print('To start your containers, run the following commands:')
+  
+  # Tell the user how to start their containers.
+  for container_name, image in network.items():
+    full_container_name = f'{username}_{container_name}'
+    print(f'docker start -i --attach {full_container_name}')
+
+
+
+
+if __name__ == '__main__':
+  main()
